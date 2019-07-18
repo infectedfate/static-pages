d48e35741e27f22948bff3fd16e526ea3097c7b8
Malinin Mikhail <infectedfate@gmail.com>
2019-07-18 16:16:20 +0300

    add user model rspec test "respond_to"

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @user = User.new(name: "Example user", email: "examlpe@mail.com") }
+  subject { @user }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:name) }
 end
