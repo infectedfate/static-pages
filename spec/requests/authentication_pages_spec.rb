476c9d3d2a8b707be6d0edaf770398546dc30669
Malinin Mikhail <infectedfate@gmail.com>
2019-07-29 00:36:10 +0300

    add spec

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -1,10 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "AuthenticationPages", type: :request do
-  describe "GET /authentication_pages" do
-    it "works! (now write some real specs)" do
-      get authentication_pages_index_path
-      expect(response).to have_http_status(200)
+  describe "signin" do
+    before { visit signin_path }
+
+    describe "with valid information" do
+      let(:user) { FactoryGirl.create(:user) }
+      before do
+        fill_in "Email",    with: user.email.upcase
+        fill_in "Password", with: user.password
+        click_button "Login"
+      end
+
+      it { should have_title(user.name) }
+      it { should have_link('Profile',     href: user_path(user)) }
+      it { should have_link('Logout',    href: logout_path) }
+      it { should_not have_link('Login', href: login_path) }
     end
   end
+
 end
