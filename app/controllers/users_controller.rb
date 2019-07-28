eec35730448859f6e2a6e733ca37900f73f9acb9
Malinin Mikhail <infectedfate@gmail.com>
2019-07-29 01:09:14 +0300

    update users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   before_action :set_user, only: %i[show destroy update edit]
+  before_action :logged_in_user, only: %i[edit update]
 
   def index
   end
@@ -19,12 +20,13 @@ class UsersController < ApplicationController
 
   def update
     if @user.update(user_params)
+      flash[:success] = "Profile updated"
       redirect_to users_path(@user)
     else
       render :edit
     end
   end
-  
+
   def edit
   end
 
@@ -50,4 +52,9 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+
+  def logged_in_user
+      redirect_to login_url, notice: "Please login." unless logged_in?
+  end
+
 end
